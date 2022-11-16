ca44aa658252c8718eb2d116ba7079dd64845e49
Aditi002-holo <aditideshpande2908@gmail.com>
2022-11-16 16:07:58 +0530

    Add code - logic

---
+++ script/app.js
@@ -0,0 +1,25 @@
+const submitBtn = document.querySelector('.submit-btn');
+const rateBtns = document.querySelectorAll('li button');
+
+let rating;
+
+rateBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        rating = Number(btn.textContent);
+        localStorage.setItem("rating", rating);
+        console.log(rating);
+    })
+})
+
+if(submitBtn) {
+    submitBtn.addEventListener('click', function() {
+        window.location = "display.html";
+    })
+}
+
+const displayRatingEl = document.querySelector('.display-rating');
+let ratingLocal = localStorage.getItem("rating");
+if(displayRatingEl) {
+    displayRatingEl.textContent = `You selected ${ratingLocal} out of 5`;
+}
+
